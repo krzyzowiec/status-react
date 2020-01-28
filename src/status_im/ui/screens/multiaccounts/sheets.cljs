e064ab0ef70c435784b3d3e171d5fc21aacdceb4
Gheorghe Pinzaru <feross95@gmail.com>
2020-01-28 15:00:05 +0300

    Update your keys screen
    Fixes #9782

---
+++ src/status_im/ui/screens/multiaccounts/sheets.cljs
@@ -0,0 +1,11 @@
+(ns status-im.ui.screens.multiaccounts.sheets
+  (:require [status-im.ui.components.list-item.views :as list-item]
+            [status-im.ui.components.react :as react]
+            [re-frame.core :as re-frame]))
+
+(defn actions-sheet []
+  [react/view
+   [list-item/list-item {:on-press #(do (re-frame/dispatch [:bottom-sheet/hide])
+                                        (re-frame/dispatch [:multiaccounts.create.ui/intro-wizard false]))
+                         :icon     :main-icons/add
+                         :title    :t/generate-a-new-key}]])
